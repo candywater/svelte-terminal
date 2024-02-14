c474d4504f2a32ac1bacd8908605cd8c2a905a20
candywater <candywater@users.noreply.github.com>
2024-02-15 02:23:00 +0800

    comment out roll up code

--- rollup.config.js
+++ rollup.config.js
@@ -1,23 +1,23 @@
-import svelte from 'rollup-plugin-svelte';
-import resolve from '@rollup/plugin-node-resolve';
-import pkg from './package.json';
-import typescript from '@rollup/plugin-typescript'
+// import svelte from 'rollup-plugin-svelte';
+// import resolve from '@rollup/plugin-node-resolve';
+// import pkg from './package.json';
+// import typescript from '@rollup/plugin-typescript'
 
-const production = !process.env.ROLLUP_WATCH;
-const name = pkg.name
-	.replace(/^(@\S+\/)?(svelte-)?(\S+)/, '$3')
-	.replace(/^\w/, m => m.toUpperCase())
-	.replace(/-\w/g, m => m[1].toUpperCase());
+// const production = !process.env.ROLLUP_WATCH;
+// const name = pkg.name
+// 	.replace(/^(@\S+\/)?(svelte-)?(\S+)/, '$3')
+// 	.replace(/^\w/, m => m.toUpperCase())
+// 	.replace(/-\w/g, m => m[1].toUpperCase());
 
-export default {
-	input: 'src/index.ts',
-	output: [
-		{ file: pkg.module, 'format': 'es' },
-		{ file: pkg.main, 'format': 'umd', name }
-	],
-	plugins: [
-		svelte(),
-		resolve(),
-		typescript({sourceMap: !production,}),
-	]
-};
+// export default {
+// 	input: 'lib/main.ts',
+// 	output: [
+// 		{ file: pkg.module, 'format': 'es' },
+// 		{ file: pkg.main, 'format': 'umd', name }
+// 	],
+// 	plugins: [
+// 		svelte(),
+// 		resolve(),
+// 		typescript({sourceMap: !production,}),
+// 	]
+// };
